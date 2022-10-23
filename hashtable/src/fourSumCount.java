3f40e9893a2942b5177ef2e899c51a5b0141dc94
yuhang <songyuhang3734@163.com>
2022-10-23 20:31:37 +0800

    fourSumCount & twoSum

---
+++ hashtable/src/fourSumCount.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ * 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -2^28 到 2^28 - 1 之间，最终结果不会超过 2^31 - 1 。
+ * 例如:
+ * 输入:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ * 输出:
+ * 2
+ * 解释:
+ * 两个元组如下:
+ * (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ * (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ *
+ *      思路：也是给定一个值，要找到符合计算法则的数，可考虑使用哈希法
+ *      一共四个数组，分成AB、CD进行遍历的话时间复杂度最低，是O(n^2)
+ *      如果分成A、BCD这种的话，时间复杂度就是O(n^3)了
+ */
+public class fourSumCount {
+
+    public static int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+
+        int total = 0;
+
+        //获取前两个数组的subSumMap
+        Map<Integer, Integer> first = getSubSumMap(nums1, nums2);
+
+        //获取后两个数组的subSumMap
+        Map<Integer, Integer> second = getSubSumMap(nums3, nums4);
+
+        for (Map.Entry<Integer, Integer> entry : second.entrySet()) {
+            Integer key = entry.getKey();
+            Integer value2 = entry.getValue();
+            int gap = -key;
+            if (first.containsKey(gap)) {
+                Integer value1 = first.get(gap);
+                total += value1 * value2;
+            }
+        }
+
+        return total;
+
+    }
+    private static Map<Integer, Integer> getSubSumMap(int[] nums1, int[] nums2) {
+        //为什么用map而不用set，是因为不仅要存值，还需要存该值出现的次数，几次就对应着几种方式
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                int subSum = nums1[i] + nums2[j];
+                if (map.containsKey(subSum)) {
+                    map.put(subSum, map.get(subSum) + 1);
+                } else {
+                    map.put(subSum, 1);
+                }
+            }
+        }
+        return map;
+    }
+
+}
---
+++ hashtable/src/twoSum.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ * 示例:
+ *      给定 nums = [2, 7, 11, 15], target = 9
+ *      因为 nums[0] + nums[1] = 2 + 7 = 9
+ *      所以返回 [0, 1]
+ *
+ *      思路：强调一下 什么时候使用哈希法，当我们需要查询一个元素是否出现过，或者一个元素是否在集合里的时候，就要第一时间想到哈希法。
+ *      本题呢，我就需要一个集合来存放我们遍历过的元素，然后在遍历数组的时候去询问这个集合，某元素是否遍历过，也就是 是否出现在这个集合。
+ *
+ * 可以这么做:给定一个数，作为目标值，我们遍历数组，每遍历一个就看一下它所需要的另外一个数在不在集合中，如果不在，那么就把这个数存到集合中，继续往后遍历做判断，一直到结束
+ *          如果期间发现当前数所需的数在集合中，说明两者之和就为目标值，返回对应下标
+ *          另外，不仅需要存这个数，还需要存它在数组中的下标，所以考虑使用map集合
+ */
+public class twoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey((target - nums[i]))) {
+                res = new int[]{map.get(target - nums[i]), i};
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return res;
+    }
+}
+
