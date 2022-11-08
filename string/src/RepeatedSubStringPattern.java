8fe0f8751496db1164eca76609485e019d20cd7e
yuhang <songyuhang3734@163.com>
2022-11-08 15:23:50 +0800

    repeatedSubStringPattern

--- string/src/RepeatedSubStringPattern.java
+++ string/src/RepeatedSubStringPattern.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * 给定一个非空的字符串，判断它是否可以由它的一个子串重复多次构成。给定的字符串只含有小写英文字母，并且长度不超过10000。
  *
@@ -32,8 +34,45 @@ public class RepeatedSubStringPattern {
 
 
     //KMP解法
+
+    /**
+     * s是重复字串 <=> s的最大相同前后缀所不包含的部分是最小重复子串 <=> s.len % (s的最大相同前后缀所不包含的部分长）= 0
+     * 所以只要求出s的最大相同前后缀所不包含部分长，判断其是不是能够被s.len整除，能被整除就能反推回去s是重复字串
+     */
     public static boolean repeatedSubStringPatternKMP(String s) {
 
-        return ;
+        int[] next = getNext(s);
+        //最大相等前后缀的长度
+        int max = next[s.length() - 1];
+
+        //排除最大相等前后缀等于0的情况，因为等于0时，s.length() % (s.length() - max) == 0恒成立
+        if (max != 0 && s.length() % (s.length() - max) == 0) {
+            return true;
+        }
+
+        return  false;
+    }
+
+    //求next数组
+    public static int[] getNext(String s) {
+
+        char[] chars = s.toCharArray();
+        int[] next = new int[s.length()];
+
+        int j = 0;//初始化前缀尾（还表示当前位置的字串的最大相等前后缀的长度）
+        next[0] = 0;//初始化next首元素
+        for (int i = 1; i < s.length(); i++) {//后缀尾
+
+            while (j > 0 && chars[i] != chars[j]) {
+                j = next[j - 1];
+            }
+
+            if (chars[i] == chars[j]) {
+                j++;
+                next[i] = j;
+            }
+        }
+
+        return next;
     }
 }
